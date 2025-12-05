245575c89cae9f63fb72f08da6bd705dbf3bf1ff
AidenJeong8 <111260867+AidenJeong8@users.noreply.github.com>
2025-12-05 09:32:49 -0800

    Create locationgraphics.py
    dummy file for now, will update !

---
+++ locationgraphics.py
@@ -0,0 +1,18 @@
+import turtle
+
+screen = turtle.Screen()
+screen.addshape("Spire.jpeg")
+
+bg = turtle.Turtle()
+bg.shape("Spire.jpeg")
+bg.left(90)
+
+def location(loc):
+  #to be written
+  
+  
+#locs = [
+ # ["entrance", 100, "light", [3, 25, "ice"], [2, 30, "light"]],
+ # ["middle", 300, "fire", [5, 65, "fighting"], [4, 80, "dark"]],
+ # ["top", 500, "psychic", [7, 75, "psychic"], [6, 100, "electric"]]
+#]
