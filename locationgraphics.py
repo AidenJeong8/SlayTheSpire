96b80fe9863bf9a1dfe6f861407809d2e2916a5e
AidenJeong8 <111260867+AidenJeong8@users.noreply.github.com>
2025-12-09 09:28:10 -0800

    finished locationgraphics.py

--- locationgraphics.py
+++ locationgraphics.py
@@ -1,18 +1,47 @@
 import turtle
 
 screen = turtle.Screen()
-screen.addshape("Spire.jpeg")
+screen.addshape("Spire.gif")
 
 bg = turtle.Turtle()
-bg.shape("Spire.jpeg")
+bg.shape("Spire.gif")
 bg.left(90)
 
+figure = turtle.Turtle()
+
+figure.shape("arrow")
+figure.color("red")
+
+entrance = turtle.Turtle()
+entrance.hideturtle()
+entrance.penup()
+entrance.goto(60,-300)
+entrance.pendown()
+entrance.color("white")
+entrance.write("entrance")
+middle = turtle.Turtle()
+middle.hideturtle()
+middle.penup()
+middle.goto(60,20)
+middle.pendown()
+middle.color("white")
+middle.write("middle")
+top = turtle.Turtle()
+top.hideturtle()
+top.penup()
+top.goto(60,100)
+top.pendown()
+top.color("white")
+top.write("top")
+
 def location(loc):
-  #to be written
-  
-  
-#locs = [
- # ["entrance", 100, "light", [3, 25, "ice"], [2, 30, "light"]],
- # ["middle", 300, "fire", [5, 65, "fighting"], [4, 80, "dark"]],
- # ["top", 500, "psychic", [7, 75, "psychic"], [6, 100, "electric"]]
-#]
+    if loc == "entrance":
+        figure.penup()
+        figure.goto(75,-280)
+    if loc == "middle":
+        figure.penup()
+        figure.goto(60,0)
+    if loc == "end":
+        figure.penup()
+        figure.goto(60,80)
+    turtle.done()
