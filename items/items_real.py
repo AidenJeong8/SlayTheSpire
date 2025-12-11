3a098c3a81d723c0bf0ca7f2f59d5ef507484c8c
AidenJeong8 <111260867+AidenJeong8@users.noreply.github.com>
2025-12-11 02:19:06 -0800

    Update items_real.py
    more comments

--- items/items_real.py
+++ items/items_real.py
@@ -1,27 +1,28 @@
 class items: #parent item
     pass
 
+# leftovers heals 20
 class leftovers(items): #heal 20
     def get_path(self):
         return "items/leftovers.gif"
     def use(self):
         print("You have used a leftover item.")
         return 20
-    
+# potions heal 30
 class potion(items): #heal 30
     def get_path(self):
         return "items/potion.gif"
     def use(self):
         print("You have used a potion.")
         return 30
-    
-class rocky_helmet(items): #deal 20 damage to player
+# helmet damages 20 to player 
+class rocky_helmet(items): 
     def get_path(self):
         return "items/rocky helmet.gif"
     def use(self):
         print("You have equipped a rocky helmet.")
         return -20
-    
+# basket heals 30
 class picnic_basket(items): #heal 30 for player
     def get_path(self):
         return "items/picnic basket.gif"
